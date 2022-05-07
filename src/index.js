27c215a4f28e3a9b904685d8c09a68812880ba62
Zach Strout <zachstrout@gmail.com>
2022-05-07 14:40:20 -0500

    Add simple fetch function
    This is just a proof-of-concept function to fetch weather data and
    return a JSON object.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+
+async function getWeather() {
+  const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=32.28&longitude=-95.34&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=America%2FChicago');
+  const json = await response.json();
+  console.log(json);
+}
+
+getWeather();
