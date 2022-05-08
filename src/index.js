eb99318b5c29b5e73708ea6290fe35a63e427a46
Zach Strout <zachstrout@gmail.com>
2022-05-07 20:29:42 -0500

    Add city finding fetch
    I realized that the weather fetch command required the lat and long of
    the location. Thankfully, the api that I am using has an ability to get
    the lat and long from a search and return options.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import './style.css';
+const search = prompt('What city');
 
-async function getWeather() {
-  const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=32.28&longitude=-95.34&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=America%2FChicago');
-  const json = await response.json();
-  console.log(json);
+async function getWeather(search) {
+  const responseCity = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${search}`);
+  const jsonCity = await responseCity.json();
+  const { latitude } = jsonCity.results[0];
+  const { longitude } = jsonCity.results[0];
+  const { timezone } = jsonCity.results[0];
+  const responseWeather = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&windspeed_unit=mph&timezone=${timezone}`);
+  const jsonWeather = await responseWeather.json();
+  console.log(jsonWeather);
 }
 
-getWeather();
+getWeather(search);
