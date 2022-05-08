791b7383749779c4418e5920797355ce27b13b81
Zach Strout <zachstrout@gmail.com>
2022-05-07 20:23:42 -0500

    Fix bug - extra semicolon

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-;  entry: ['@babel/polyfill', './src/index.js'],
+  entry: ['@babel/polyfill', './src/index.js'],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
